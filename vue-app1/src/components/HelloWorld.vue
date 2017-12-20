3ac81fc6f6c59ea7bea3288cb92c8895dcd26233
Vitaliy <vitaliy@altsolution.net>
2017-12-20 23:07:38 +0200

    test commit

---
+++ vue-app1/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Start',
+  computed: {
+    
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  margin-top: 60px;
+}
+</style>
